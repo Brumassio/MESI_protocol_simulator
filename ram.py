cd1b39e9f955157ccaa33731b964fce937c1787c
Brumassio <dibrunspeed@gmail.com>
2022-10-14 00:49:26 -0300

    Classe ram atualizada 14/10 (era só fazer um vetor !!)

--- ram.py
+++ ram.py
@@ -1,5 +1,12 @@
 class ram:
     def __init__(self,dado):
         # self.linha = linha
+        self.tamRam = 1024
         self.dado = dado
-        
+
+    def mostrarRam(self): #imprime a ram 
+        for i in range (self.tamRam):
+            print(i, self.dado[i])
+
+    def leituraRAM(self,pos):
+        return self.dado[pos]
