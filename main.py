6008796afc26bfbaf794eb452cc3b9f3a2e56ebc
Brumassio <dibrunspeed@gmail.com>
2022-10-21 17:54:03 -0300

    classe main atualizadinha -> 21/10

--- main.py
+++ main.py
@@ -18,27 +18,27 @@ print(cacheProcess1.dados)
 cacheProcess2 = cache() 
 cacheProcess3 = cache() 
 
-resposta = 0 
-
+resposta = 0
+ 
 resposta = int(input("Escolha qual processador será usado: "))
 operacao = input("Escolha qual operação deseja realizar: ")
 posicao = int(input("Digite a posição: "))
 print(resposta)
 if resposta == 1:
     if operacao == "read":
-        cacheProcess1.read(posicao,memRam)
+        cacheProcess1.read(posicao,memRam,cacheProcess2,cacheProcess3)
     elif operacao == "write":
-        cacheProcess1.write(posicao,memRam)
+        cacheProcess1.write(posicao,memRam,cacheProcess2,cacheProcess3)
 elif resposta == 2:
     if operacao == "read":
-        cacheProcess2.read(posicao,memRam)
+        cacheProcess2.read(posicao,memRam,cacheProcess1,cacheProcess3)
     elif operacao == "write":
-        cacheProcess2.write(posicao,memRam)
+        cacheProcess2.write(posicao,memRam, cacheProcess1,cacheProcess3)
 elif resposta == 3:
     if operacao == "read":
-        cacheProcess3.read(posicao,memRam)           
+        cacheProcess3.read(posicao,memRam, cacheProcess1,cacheProcess2)           
     elif operacao == "write":
-        cacheProcess3.write(posicao,memRam)
+        cacheProcess3.write(posicao,memRam, cacheProcess1,cacheProcess2)
 
 
 
